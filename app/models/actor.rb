09dc880c4b52193103e449aaa2b4aa6a4c3173fd
Kyle Baum <username@users.noreply.github.com>
2016-11-14 17:43:01 -0600

    with helper methods

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,4 +4,8 @@ validates :name, :presence => true, :uniqueness => { :scope => :dob }
 # - dob: no rules
 # - bio: no rules
 # - image_url: no rules
+
+has_many :characters
+has_many :movies
+has_many :directors
 end
